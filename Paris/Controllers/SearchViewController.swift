cb24175a544ed05a8259f4cfd9a0593206f707b2
thaxz <thais.monteiroc@ufpe.br>
2022-09-14 09:37:46 -0300

    working on search screen

--- Paris/Controllers/SearchViewController.swift
+++ Paris/Controllers/SearchViewController.swift
@@ -6,15 +6,67 @@
 //
 
 import UIKit
+import SwiftUI
 
 // Controller da tela "Busca"
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
 
+    // Storing and instantiating what we are going to get from database
+    
+    var searchRestaurants: [Restaurant] = []
+    var searchTours: [Tour] = []
+    
+    // Instantiating searchController
+    let searchController = UISearchController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpSearchController()
+    }
+    
+    func setHierarchy(){
+        
+        // MARK: Adicionando componentes na view
+        // A ordem importa!
+        // View
+        
+    }
+    
+    func changingAttributes(){
+        
+    }
+    
+    func setUpConstraints () {
+        
+    }
+    
+    func btConfiguration(){
+        
+        
+    }
+    
+    // Closing keyboard as you touch anywhere else on the screen
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        //lbSearch.isHidden = false
+    }
+    
+    // Updating results as soon as you type something
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        //lbSearch.isHidden = true
+        guard let title = searchController.searchBar.text else {return}
+        
+//        Task {
+//
+//            self.searchMovies = await Movie.searchMoviesAPI(title: title)
+//            tableViewSearch.reloadData()
+//        }
+        
     }
     
 
@@ -24,7 +76,19 @@ class SearchViewController: UIViewController {
         // Title
         self.navigationItem.title = "O que você procura?"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Setting up searchController
+        
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search"
+        
         // Chamar outras funções que montem a tela aqui
+        setHierarchy()
+        changingAttributes()
+        setUpConstraints()
+        btConfiguration()
+        
     }
     
     
